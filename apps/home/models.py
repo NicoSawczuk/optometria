4331b41da62d9ecb25b0d3cfff4dd332d0bc5bbc
Javier <kachuka99@gmail.com>
2020-11-19 09:39:45 -0300

    Creacion de nuevos modelos
    Se agregaron los modelos:
     * TIpoDePago
     * EstadoPedido
     * ValorCaracteristicaProducto
     * CaracteristicaProducto
     * Producto
     * Pedido

--- apps/home/models.py
+++ apps/home/models.py
@@ -65,3 +65,87 @@ class Turno(models.Model):
     def __str__(self):
         return 'Turno '+ self.paciente.nombre +' '+ self.paciente.apellido
     
+class TipoDePago(models.Model):
+    id = models.AutoField(primary_key = True)
+    descripcion = models.CharField(max_length = 60, blank = False, null = True)
+
+    class Meta:
+        verbose_name = 'TipoDePago'
+        verbose_name_plural = 'TiposDePagos'
+        ordering = ['descripcion']
+    
+    def __str__(self):
+        return self.descripcion
+    
+class EstadoPedido(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 60, blank = False, null = True)
+    descripcion = models.CharField(max_length = 60, blank = False, null = True)
+
+    class Meta:
+        verbose_name = 'EstadoPedido'
+        verbose_name_plural = 'EstadosPedidos'
+        ordering = ['nombre']
+    
+    def __str__(self):
+        return self.nombre
+
+class ValorCaracteristicaProducto(models.Model):
+    id = models.AutoField(primary_key = True)
+    valor = models.CharField(max_length = 60, blank = False, null = True)
+
+    class Meta:
+        verbose_name = 'ValorCaracteristicaProducto'
+        verbose_name_plural = 'ValorCaracteristicaProductos'
+        ordering = ['valor']
+    
+    def __str__(self):
+        return self.valor
+    
+class CaracteristicaProducto(models.Model):
+    id = models.AutoField(primary_key = True)
+    caracteristica = models.CharField(max_length = 60, blank = False, null = True)
+    valor = models.ManyToManyField(ValorCaracteristicaProducto, verbose_name="ValorCaracteristicaProducto")
+
+
+    class Meta:
+        verbose_name = 'CaracteristicaProducto'
+        verbose_name_plural = 'CaracteristicasProductos'
+        ordering = ['caracteristica']
+    
+    def __str__(self):
+        return self.caracteristica
+    
+    
+class Producto(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 60, blank = False, null = True)
+    descripcion = models.CharField(max_length = 120, blank = False, null = True)
+    caracteristicaProducto = models.ForeignKey(CaracteristicaProducto, verbose_name="CaracteristicaProducto", on_delete=models.CASCADE)
+    valorCaracteristicaProducto = models.ForeignKey(ValorCaracteristicaProducto, verbose_name="ValorCaracteristicaProducto", on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=5, decimal_places=2, blank = True, null = True)
+
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
+        ordering = ['nombre']
+    
+    def __str__(self):
+        return self.nombre + ' ' + self.caracteristicaProducto.caracteristica + ' ' + self.valorCaracteristicaProducto.valor
+        
+class Pedido(models.Model):
+    id = models.AutoField(primary_key = True)
+    paciente = models.ForeignKey(Paciente, verbose_name="Paciente", on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, verbose_name="Producto", on_delete=models.CASCADE)
+    subtotal = models.DecimalField(max_digits=5, decimal_places=2)
+    tipoDePago = models.ForeignKey(TipoDePago, verbose_name="TipoDePago", on_delete=models.CASCADE)
+    estado = models.ForeignKey(EstadoPedido, verbose_name="EstadoPedido", on_delete=models.CASCADE)
+    
+    
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+        ordering = ['producto']
+    
+    def __str__(self):
+        return 'Pedido '+ self.paciente.nombre +' '+ self.paciente.apellido + ' (' + self.producto.nombre + ') '
