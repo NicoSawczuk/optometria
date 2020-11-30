bfa6787a9c56218b3689e927c8c4d0c4a6faac76
Javier <kachuka99@gmail.com>
2020-11-30 18:47:17 -0300

    get  cantidad vendida de productos

--- apps/home/models.py
+++ apps/home/models.py
@@ -157,6 +157,10 @@ class Producto(models.Model):
     
     def __str__(self):
         return self.nombre + ' ' + self.caracteristicaProducto.caracteristica + ' ' + self.valorCaracteristicaProducto.valor
+
+    def getCantidadVendida (self):
+        return Pedido.objects.filter(producto__id=self.id,estado__nombre='Finalizado').count()
+
         
 class Pedido(models.Model):
     id = models.AutoField(primary_key = True)
